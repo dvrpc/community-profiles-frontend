c733c9b4e65246e377d9d2d5f9a86e80213d64ef
ckirby98 <colin@kirby.net>
2025-10-01 16:29:53 -0400

    wip region data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
 import Hero from "@/components/Hero/Hero";
+import { API_BASE_URL } from "@/consts";
+import { ProfileContent, RegionData } from "@/types";
 
-export default function Home() {
-  return <Hero />;
+export default async function Home() {
+
+  const regionResponse = await fetch(
+    `${API_BASE_URL}/profile/region`
+  );
+  const countyData = (await regionResponse.json()) as RegionData;
+  const contentResponse = await fetch(
+    `${API_BASE_URL}/content/region`
+  );
+  const content = (await contentResponse.json()) as ProfileContent;
+
+  return (
+    <div>
+      <Hero
+        geographyName={'DVRPC Region'}
+        profileData={countyData}
+        geoLevel="county"
+      />
+      <Content content={content} data={countyData} geoLevel="county" />
+    </div>
+  );
 }
